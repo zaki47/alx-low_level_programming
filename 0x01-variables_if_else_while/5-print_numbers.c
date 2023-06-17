46d3627ad63a80a40571f15151713b899adc65d8
Zakaria Touras <zakari4touras@gmail.com>
2023-06-17 09:13:33 -0700

    nbr 0~9, for, printf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print digit 0-10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
