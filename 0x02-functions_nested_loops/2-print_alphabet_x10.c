e66e8bf0b82a93399e57efd791bc633bcb7e4675
Zakaria Touras <zakari4touras@gmail.com>
2023-06-21 18:57:37 -0700

    alphabet_low*10 using for

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet  lowercase *10
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
