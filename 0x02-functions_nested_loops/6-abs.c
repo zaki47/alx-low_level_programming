482383dccf094c0755320301f528fbc7ec1fe090
Zakaria Touras <zakari4touras@gmail.com>
2023-06-21 19:43:25 -0700

    if looop

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - function computes the absolute value of an integer.
+ * @n: int number
+ * Return: 1 or 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
