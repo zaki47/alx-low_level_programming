9dfd2a2b817c2f247ab874e3cc9519c36eea267b
Zakaria Touras <zakari4touras@gmail.com>
2023-06-25 22:31:34 -0700

    fixed betty

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,11 +15,11 @@ void print_square(int size)
 		_putchar('\n');
 	}
 	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
+			_putchar('#');
 		}
+		_putchar('\n');
+	}
 }
