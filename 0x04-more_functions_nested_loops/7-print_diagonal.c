19013323e4e6a667108e3c59901c55bc0c427359
Zakaria Touras <zakari4touras@gmail.com>
2023-06-25 22:00:10 -0700

    now fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 			{
@@ -20,8 +20,8 @@ void print_diagonal(int n)
 			}
 			_putchar('\\');
 			_putchar('\n');
-		}		
-	}	
+		}
+	}
 	else
 	{
 		_putchar('\n');
