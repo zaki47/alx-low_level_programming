254aa2e79548147e9f617c8052ff54cb92d7f416
Zakaria Touras <zakari4touras@gmail.com>
2023-06-25 20:46:17 -0700

    fixed task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function draws dialgonal line
+ * @n: number of time should print '\'
+ * Return: int
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}		
+	}	
+	else
+	{
+		_putchar('\n');
+	}
+}
