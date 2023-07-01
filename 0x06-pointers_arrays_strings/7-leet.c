33caabb018a1ec8189aecfa580142367fb0dc939
Zakaria Touras <zakari4touras@gmail.com>
2023-06-30 19:42:55 -0700

    check 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - a string into 1337 or leetspeak.
+ *@str: string.
+ * Return: str.
+ */
+
+char *leet(char *str)
+{
+	char *letters = "aAeEoOtTlL";
+	char *leets = "4433007711";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = leets[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
