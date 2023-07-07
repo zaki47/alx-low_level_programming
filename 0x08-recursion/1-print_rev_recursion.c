b3a4a564c0ff22ce366311c78c7ab2101bb28a95
Zakaria Touras <zakari4touras@gmail.com>
2023-07-06 19:19:29 -0700

    reconst func2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string.
- * Return: len of a string.
-*/
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string
+ */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return (0);
+		return;
 	}
-	return (1 + _strlen_recursion(s + 1));
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
