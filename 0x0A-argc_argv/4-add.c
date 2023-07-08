c588b8f7709710538702489ae45fd94381bab12f
Zakaria Touras <zakari4touras@gmail.com>
2023-07-07 19:17:53 -0700

    usubg gpt

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,22 +2,37 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers.
- * @argc: int.
- * @argv: arr to str.
- * Return: 1 / 0.
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int result;
+	int i, n, sum = 0;
+	char *flag;
 
-	if (argc != 3)
+	if (argc < 2)
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
+		return (0);
 	}
-	result = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", result);
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
 	return (0);
 }
