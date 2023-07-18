ebd950003facb63f0862cb648f85077989152dd4
Zakaria Touras <zakari4touras@gmail.com>
2023-07-17 19:32:28 -0700

    rename file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 3_FUNCTION_LIKE_MACRO_H
-#define 3_FUNCTION_LIKE_MACRO_H
+#ifndef __3_FUNCTION_LIKE_MACRO_H__
+#define __3_FUNCTION_LIKE_MACRO_H__
 
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif
+#endif 
