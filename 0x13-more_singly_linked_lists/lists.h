288e7812edd6219087b665cf95ab16f006524396
Zakaria Touras <zakari4touras@gmail.com>
2023-08-01 19:03:34 -0700

    fix ifdefine

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,20 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stddef.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -18,3 +35,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
+#endif
