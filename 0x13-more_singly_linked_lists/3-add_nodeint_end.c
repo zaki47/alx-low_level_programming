7f97bf445c95841981a4701e59dd745f8e521b1f
Zakaria Touras <zakari4touras@gmail.com>
2023-08-02 19:27:36 -0700

    task 03 fix betty

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,12 +11,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *ptr = *head;
 	listint_t *temp = malloc(sizeof(listint_t));
+
 	if (!temp)
 		return (NULL);
-	
+
 	temp->n = n;
 	temp->next = NULL;
-	
+
 	if (!*head)
 	{
 		*head = temp;
