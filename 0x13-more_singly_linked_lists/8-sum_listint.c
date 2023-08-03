7d600e4f4a061fae41c24ff86e5b849bb4a4429d
Zakaria Touras <zakari4touras@gmail.com>
2023-08-02 19:40:46 -0700

    task 8 checker2

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data (n) of
- *	a listint_t linked list.
+ * sum_listint -  returns the sum of all the data (n) of
+ *  a listint_t linked list.
  * @head: head pointer.
  * Return: the sum of the data.
  */
@@ -10,8 +10,7 @@
 int sum_listint(listint_t *head)
 {
 	listint_t *curr;
-	int n; 
-	sum = 0;
+	int n, sum = 0;
 
 	if (!head)
 		return (0);
