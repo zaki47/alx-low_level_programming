9811010dacb72701e98fcba24c5632787a7e96aa
Zakaria Touras <zakari4touras@gmail.com>
2023-10-06 19:31:26 -0700

    task 3 fix .h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -39,7 +39,7 @@ hash_table_t *hash_table_create(unsigned long int);
 unsigned long int hash_djb2(const unsigned char *);
 unsigned long int key_index(const unsigned char*, unsigned long int);
 int hash_table_set(hash_table_t*, const char*, const char*);
-char *hash_table_get(const hash_table_t*, const char*);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *);
 void hash_table_delete(hash_table_t *);
 
