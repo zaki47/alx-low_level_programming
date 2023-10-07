0794cd0587a45d4481f5e4791402c4c0891af911
Zakaria Touras <zakari4touras@gmail.com>
2023-10-06 19:46:53 -0700

    task 3 fix .h

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,100 +1,51 @@
 #include "hash_tables.h"
 
-/*******
- * _strdup - Duplicates a string in a newly allocated memo
- *
- * @s: The string to be duplicated
- *
- * Return: A pointer to the newly allocated duplicated string, or NULL
- *         if memory allocation fails or if @s is NULL.
- */
-
-char *_strdup(const char *s)
-{
-	int i;
-	char* New;
-
-	i = 0;
-	while (s[i])
-		i++;
-	new = malloc(sizeof(char) * (i + 1));
-	if (!New)
-		return (0);
-	i = 0;
-	while (s[i])
-	{
-		New[i] = s[i];
-		i++;
-	}
-	New[i] = 0;
-	return (New);
-}
-
 /**
- * _strcmp - Compares two strings character by char
+ * hash_table_set - Adds an element to the hash table.
  *
- * @s1: The 1st string to compare
- * @s2: The 2nd string to compare
+ * @ht: Pointer to the hash table.
  *
- * Return: int
- */
-int _strcmp(char* s1, char* s2)
-{
-	int i;
-
-	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i])
-		i++;
-	return (s1[i] - s2[i]);
-}
-
-/**
- * hash_table_set - Adds or updates a key-value pair in a hash table.
+ * @key: Key to set in the hash table.
  *
- * @ht: A pointer to the hash table.
- * @key: The key to be added or updated.
- * @value: The value associated with the key.
+ * @value: Value to save in the hash table.
  *
- * Return: 1 if the operation succeeds, 0 otherwise.
+ * Return: 1 if it succeeded, 0 otherwise.
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int key_idx;
-	hash_node_t *temp;
-	hash_node_t *item;
+	unsigned long int index = 0;
+	hash_node_t *node = NULL, *add_node = NULL, *tmp = NULL;
 
-	if (!key || !ht || !value)
-		return (0);
-	key_idx = key_index((const unsigned char *)key, ht->size);
-	item = malloc(sizeof(hash_node_t));
-	if (!item)
-		return (0);
-	item->key = _strdup(key);
-	item->value = _strdup(value);
-	if (ht->array[key_idx] == NULL)
+	if (!key || !ht || !ht->array)
+		return (false);
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	if (!node)
 	{
-		ht->array[key_idx] = item;
-		item->next = NULL;
+		node = malloc(sizeof(hash_node_t));
+		if (!node)
+			return (false);
+		node->key = strdup(key), node->value = strdup(value);
+		node->next = NULL;
+		ht->array[index] = node;
+		return (true);
 	}
-	else
-	{
-		temp = ht->array[key_idx];
-		while (temp)
+
+	for (tmp = node; tmp; tmp = tmp->next)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			if (!_strcmp(temp->key, item->key))
-			{
-				free(temp->value);
-				temp->value = item->value;
-				free(item->key);
-				free(item);
-				return (1);
-			}
-			temp = temp->next;
+			free(tmp->value), tmp->value = strdup(value);
+			return (true);
 		}
-		temp = ht->array[key_idx];
-		ht->array[key_idx] = item;
-		item->next = temp;
-	}
-	return (1);
+	add_node = malloc(sizeof(hash_node_t));
+	if (!add_node)
+		return (false);
+	add_node->key = strdup(key), add_node->value = strdup(value);
+	add_node->next = node;
+	ht->array[index] = add_node;
+
+	return (true);
 }
